7ae1c64dbba913aa1b302ec36290ff5e2a64a2a9
James Lin <jl3782@columbia.edu>
2015-02-24 17:55:22 -0500

    fix.feat(Client.py): echo server working and timing issue solved hackily

--- Client.py
+++ Client.py
@@ -1,25 +1,102 @@
 import socket
 import signal
+import time
+import threading
 
-
-SIG_INT = 2
+RECV_SIZE = 1024
+HOST = 'localhost'
+HEARTBEAT = 1
+PORT = 0
+USERNAME = ''
 
 def ctrl_c_handler(signum, frame):
-    exit()
+    exit(0)
 
-signal.signal(SIG_INT, ctrl_c_handler)
+def alarm_handler():
+    global HOST
+    global PORT
+    global USERNAME
 
-# give host and port information
-HOST = 'localhost'
-PORT = int(raw_input('PORT NUMBER: '))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    sock.sendall('LIVE')
+    time.sleep(.1)
+    sock.sendall(USERNAME)
+    reply_code = sock.recv(RECV_SIZE)
+    description = sock.recv(RECV_SIZE)
 
-# basic client socket connection
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if reply_code == 'DEAD':
+        print description
+        sock.close()
+        exit(0)
 
-s.connect((HOST, PORT))
-while 1:
-    data = s.recv(1024)
-    userInput = raw_input('>' + data)
-    s.send(userInput)
-    
-s.close()
+    sock.close()
+
+    time.sleep(HEARTBEAT)
+    t = threading.Thread(target=alarm_handler)
+    t.daemon = True
+    t.start()
+
+    return(0)
+
+def main():
+
+    # give host and port information
+    global PORT
+    global USERNAME
+
+    PORT = int(raw_input('PORT NUMBER: '))
+
+    # basic client socket connection
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+
+    sock.send('HELO')
+    reply_code = sock.recv(RECV_SIZE)
+    description = sock.recv(RECV_SIZE)
+
+    while (reply_code != 'SUCC') and (reply_code != 'FAIL'):
+        userInput = raw_input('>' + description)
+        sock.sendall(userInput)
+        reply_code = sock.recv(RECV_SIZE)
+        description = sock.recv(RECV_SIZE)
+
+    if reply_code == 'FAIL':
+        print description
+        sock.close()
+        exit(0)
+    else:
+        USERNAME = sock.recv(RECV_SIZE)
+        sock.close()
+
+    logged_in = True
+    t = threading.Thread(target=alarm_handler)
+    t.daemon = True
+    t.start()
+
+    while logged_in:
+
+        userInput = raw_input(description + '\n>')
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        sock.sendall('CMND')
+        time.sleep(.3)
+        sock.sendall(userInput)
+        reply_code = sock.recv(RECV_SIZE)
+        description = sock.recv(RECV_SIZE)
+        sock.close()
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, ctrl_c_handler)
+    signal.signal(signal.SIGALRM, alarm_handler)
+    main()
+
+# log in
+# remember who you are
+# connect and send heartbeat with username every X seconds
+# can also connect and send commands with username
+# log out means stop sending heartbeats
